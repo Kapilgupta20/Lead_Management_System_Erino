82d9bbb6c843915d2f3d265bcc26e7fd45e65ce1
Kapilgupta20 <ofkapilgupta1@gmail.com>
2025-08-25 17:13:36 +0530

    Connection error resolve

--- frontend/src/utils/authapi.js
+++ frontend/src/utils/authapi.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const VITE_API_URL = import.meta.env.VITE_API_URL || 'http://localhost:4000';
+const VITE_API_URL = import.meta.env.VITE_API_URL;
 
 async function handleResponse(promise) {
   try {
